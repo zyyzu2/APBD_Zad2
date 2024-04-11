031a904da17192a8dabe7f2eeff1f46bb781eaa7
Mateusz Zych <s24854@pjwstk.edu.pl>
2024-04-11 19:14:27 +0200

    Dododanie osobnej klasy do walidacji danych

--- LegacyApp/UserService.cs
+++ LegacyApp/UserService.cs
@@ -6,25 +6,10 @@ namespace LegacyApp
     {
         public bool AddUser(string firstName, string lastName, string email, DateTime dateOfBirth, int clientId)
         {
-            if (string.IsNullOrEmpty(firstName) || string.IsNullOrEmpty(lastName))
+            if (!UserValidation.Validate(firstName,lastName, email, dateOfBirth))
             {
                 return false;
             }
-
-            if (!email.Contains("@") && !email.Contains("."))
-            {
-                return false;
-            }
-
-            var now = DateTime.Now;
-            int age = now.Year - dateOfBirth.Year;
-            if (now.Month < dateOfBirth.Month || (now.Month == dateOfBirth.Month && now.Day < dateOfBirth.Day)) age--;
-
-            if (age < 21)
-            {
-                return false;
-            }
-
             var clientRepository = new ClientRepository();
             var client = clientRepository.GetById(clientId);
 
@@ -36,35 +21,33 @@ namespace LegacyApp
                 FirstName = firstName,
                 LastName = lastName
             };
-            
-            if (client.Type == "VeryImportantClient")
+
+            switch (client.Type)
             {
-                user.HasCreditLimit = false;
-            }
-            else if (client.Type == "ImportantClient")
-            {
-                using (var userCreditService = new UserCreditService())
+                case "VeryImportantClient":
+                    user.HasCreditLimit = false;
+                    break;
+                case "ImportantClient":
                 {
+                    using var userCreditService = new UserCreditService();
                     int creditLimit = userCreditService.GetCreditLimit(user.LastName, user.DateOfBirth);
                     creditLimit = creditLimit * 2;
                     user.CreditLimit = creditLimit;
+                    break;
                 }
-            }
-            else
-            {
-                user.HasCreditLimit = true;
-                using (var userCreditService = new UserCreditService())
+                default:
                 {
+                    user.HasCreditLimit = true;
+                    using var userCreditService = new UserCreditService();
                     int creditLimit = userCreditService.GetCreditLimit(user.LastName, user.DateOfBirth);
                     user.CreditLimit = creditLimit;
+                    break;
                 }
             }
-            
             if (user.HasCreditLimit && user.CreditLimit < 500)
             {
                 return false;
             }
-
             UserDataAccess.AddUser(user);
             return true;
         }
---
+++ LegacyApp/UserValidation.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace LegacyApp;
+
+public class UserValidation
+{
+    public static bool Validate(string firstName, string lastName, string email, DateTime dateOfBirth)
+    {
+        if (string.IsNullOrEmpty(firstName) || string.IsNullOrEmpty(lastName)) return false;
+        if (!email.Contains('@') && !email.Contains('.')) return false;
+        var now = DateTime.Now;
+        int age = now.Year - dateOfBirth.Year;
+        if (now.Month < dateOfBirth.Month || (now.Month == dateOfBirth.Month && now.Day < dateOfBirth.Day)) age--;
+        if (age < 21) return false;
+
+        return true;
+    }
+}
