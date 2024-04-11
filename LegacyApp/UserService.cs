0da82d89f4ccc9f431efd1d90c5361a7db8f508c
Mateusz Zych <s24854@pjwstk.edu.pl>
2024-04-11 19:36:41 +0200

    Przeniesiono metodę validate do klasy UserService

--- LegacyApp/UserService.cs
+++ LegacyApp/UserService.cs
@@ -6,7 +6,7 @@ namespace LegacyApp
     {
         public bool AddUser(string firstName, string lastName, string email, DateTime dateOfBirth, int clientId)
         {
-            if (!UserValidation.Validate(firstName,lastName, email, dateOfBirth))
+            if (!Validate(firstName,lastName, email, dateOfBirth))
             {
                 return false;
             }
@@ -49,6 +49,17 @@ namespace LegacyApp
                 return false;
             }
             UserDataAccess.AddUser(user);
+            return true;
+        }
+        public static bool Validate(string firstName, string lastName, string email, DateTime dateOfBirth)
+        {
+            if (string.IsNullOrEmpty(firstName) || string.IsNullOrEmpty(lastName)) return false;
+            if (!email.Contains('@') && !email.Contains('.')) return false;
+            var now = DateTime.Now;
+            int age = now.Year - dateOfBirth.Year;
+            if (now.Month < dateOfBirth.Month || (now.Month == dateOfBirth.Month && now.Day < dateOfBirth.Day)) age--;
+            if (age < 21) return false;
+
             return true;
         }
     }
